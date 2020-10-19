18a76679537439b0b9fc17971deac58da40d9f71
Snehasish Dhar <dsnehasish74@gmail.com>
2020-10-19 13:48:03 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import IconLabelTabs from './components/nav.js'
-
-import Home from './home';
 import FinalGlobe from './FinalGlobe'
-import About from './aboutus.js'
 
 function App() {
     return (
     <div>
-      <Router>
-        <IconLabelTabs/>
-          <Switch>
-            <Route  exact path="/" component={ Home } />
-            <Route exact path="/FinalGlobe" component={ FinalGlobe } />
-            <Route exact path="/aboutus" component={ About } />
-          </Switch>
-      </Router>
-
+        <FinalGlobe/>
     </div>
   );
 }
