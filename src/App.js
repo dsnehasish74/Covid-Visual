f54a807b47d000f0751e97aa20803d997782e35f
Snehasish Dhar <dsnehasish74@gmail.com>
2020-10-19 13:48:19 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import IconLabelTabs from './components/nav.js'
 import FinalGlobe from './FinalGlobe'
 
 function App() {
